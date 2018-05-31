d087248d8da0ef2c348ac38b244286ed10906d96
Tolga Arslan <tolgaarslan0@gmail.com>
2018-05-31 12:06:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function happyHolidayTo (name, holiday){
 }
 
 function holidayCountdown (holiday, days){
-  return "It's " + days " days until " + holiday + "!";
+  return "It*s " + days " days until " + holiday + "!";
 }
